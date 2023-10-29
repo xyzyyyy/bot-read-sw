a7e8888dbbad5221b35fb975acc3125deda01256
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-10-30 00:29:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-	name: 'Bolaxd', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
-	owner: '6287839067186', // Ubah Nomor ke Nomor owner
-	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
+	name: 'Ryhard', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
+	owner: '62882008211320', // Ubah Nomor ke Nomor owner
+	autoread: false, // Ubah Ini untuk read sw == true, untuk tidak maka == false
 	faston: 1000 // Semakin dikit nilai nya, maka semakin cepat read SW
 }
 export default config;
